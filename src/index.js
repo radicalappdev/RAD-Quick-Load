6c6eaaf6c7d1e185e197bef24952ce5919ff8292
Joseph Simpson <joe@radicalappdev.com>
2023-01-05 06:52:26 -0500

    Dead end
    I can archive this repo

--- src/index.js
+++ src/index.js
@@ -5,3 +5,21 @@ const btn = document.querySelector("button");
 btn.onclick = function () {
   alert("You ran some JavaScript");
 };
+
+const filePicker = document.getElementById("filePicker");
+filePicker.addEventListener("change", handleFiles, false);
+function handleFiles() {
+  const fileList = this.files;
+  let reader = new FileReader();
+  reader.onload = onReaderLoad;
+  let data = reader.readAsText(fileList[0]);
+
+  function onReaderLoad(event) {
+    console.log(event.target.result);
+    var obj = JSON.parse(event.target.result);
+    console.log(obj);
+  }
+
+  console.log(fileList); /* now you can work with the file list */
+  console.log(data); /* now you can work with the file list */
+}
