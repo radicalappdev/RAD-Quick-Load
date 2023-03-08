60cba2e2ae267bcf985c1632e8cde713249afa76
Joseph Simpson <joe@radicalappdev.com>
2023-03-08 13:23:52 -0500

    Playing around with file loading and pico

--- src/index.js
+++ src/index.js
@@ -1,25 +1,17 @@
 //Here we're importing items we'll need. You can add other imports here.
 
-//The first function. Remove this.
-const btn = document.querySelector("button");
-btn.onclick = function () {
-  alert("You ran some JavaScript");
-};
+const fileInput = document.getElementById("fileInput");
 
-const filePicker = document.getElementById("filePicker");
-filePicker.addEventListener("change", handleFiles, false);
-function handleFiles() {
-  const fileList = this.files;
-  let reader = new FileReader();
-  reader.onload = onReaderLoad;
-  let data = reader.readAsText(fileList[0]);
+fileInput.addEventListener("change", function (event) {
+  const file = event.target.files[0];
 
-  function onReaderLoad(event) {
-    console.log(event.target.result);
-    var obj = JSON.parse(event.target.result);
-    console.log(obj);
-  }
+  const reader = new FileReader();
+  reader.onload = function (event) {
+    const contents = event.target.result;
+    const json = JSON.parse(contents);
+    console.log(json);
 
-  console.log(fileList); /* now you can work with the file list */
-  console.log(data); /* now you can work with the file list */
-}
+    var loaded = JSON.parse(contents);
+  };
+  reader.readAsText(file);
+});
