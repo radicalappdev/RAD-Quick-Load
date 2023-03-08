db4a8db07bba04b89f6c385e60b9634865718b49
Joseph Simpson <joe@radicalappdev.com>
2023-03-08 14:15:50 -0500

    Minor code clean up

--- src/index.js
+++ src/index.js
@@ -5,19 +5,3 @@ import App from "./App.vue";
 
 const app = createApp(App);
 app.mount("#app");
-
-// const fileInput = document.getElementById("fileInput");
-
-// fileInput.addEventListener("change", function (event) {
-//   const file = event.target.files[0];
-
-//   const reader = new FileReader();
-//   reader.onload = function (event) {
-//     const contents = event.target.result;
-//     const json = JSON.parse(contents);
-//     console.log(json);
-
-//     var loaded = JSON.parse(contents);
-//   };
-//   reader.readAsText(file);
-// });
